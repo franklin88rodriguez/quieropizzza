27db1d8a9d05300442ec4f02ac130ef720c215b0
franklin88rodriguez <franklin88rodriguez@gmail.com>
2019-02-17 20:56:56 -0600

    Cambios entre pagina, Home y Lista de asignaturas

---
+++ CargaAcademicaInformatica/CargaAcademicaInformatica/Models/CargaAcademicaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CargaAcademicaInformatica.Models
+{
+    public class CargaAcademicaModel
+    {
+        public string NombreClase { get; set; }
+        public int SeccionClase { get; set; }
+
+
+    }
+}
