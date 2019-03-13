b05bf714055e7e95b35d29e5af2d7624b3190da8
franklin88rodriguez <franklin88rodriguez@gmail.com>
2019-03-13 15:28:34 -0600

    Ultimo con validaciones e imagenes

--- CargaAcademicaInformatica/CargaAcademica.BL/CargaAcademicaBL.cs
+++ CargaAcademicaInformatica/CargaAcademica.BL/CargaAcademicaBL.cs
@@ -9,37 +9,80 @@ namespace CargaAcademica.BL
     public class CargaAcademicaBL
     {
         Contexto _contexto;
+        public List<Asignatura> ListadeAsignaturas { get; set; }
+
         public CargaAcademicaBL()
         {
             _contexto = new Contexto();
+            ListadeAsignaturas = new List<Asignatura>();
+
         }
-       public List<Asignatura> ObtenerAsignaturas()
+
+        public List<Asignatura> ObtenerAsignatura()
         {
-
-            _contexto.Asignaturas.ToList();
-
-            var Asignatura1 = new Asignatura();
-
-            Asignatura1.Seccion = "1600";
-            Asignatura1.NumeroEdificio = "5";
-            Asignatura1.NombreAsignatura = "Lenguaje de Programacion 3";
-            Asignatura1.NombreCatedratico = "Franklin Xavier Rodriguez Santos";
-
-            var Asignatura2 = new Asignatura();
-
-            Asignatura2.Seccion = "1700";
-            Asignatura2.NumeroEdificio = "5";
-            Asignatura2.NombreAsignatura = "Base de Datos 1";
-            Asignatura2.NombreCatedratico = "Edgar Antonio Zuniga";
-
-
-
-            var ListaAsignaturas = new List<Asignatura>();
-
-            ListaAsignaturas.Add(Asignatura1);
-            ListaAsignaturas.Add(Asignatura2);
-
-            return ListaAsignaturas;
+            ListadeAsignaturas = _contexto.Asignaturas.ToList();
+            return ListadeAsignaturas;
         }
-    }
+
+        public Asignatura ObtenerAsignatura(string seccion)
+        {
+            var producto = _contexto.Asignaturas.Find(seccion);
+
+            return producto;
+        }
+
+        public void GuardarAsignatura(Asignatura asignatura)
+        {
+            if (asignatura.Seccion == 0)
+            {
+                _contexto.Asignaturas.Add(asignatura);
+            }
+            else
+            {
+                var asignaturaExistente = _contexto.Asignaturas.Find(asignatura.Seccion);
+                asignaturaExistente.NumeroEdificio = asignatura.NumeroEdificio;
+                asignaturaExistente.NombreAsignatura = asignatura.NombreAsignatura;
+            }
+
+            _contexto.SaveChanges();
+        }
+
+        public void EliminarAsignatura(string seccion)
+        {
+            var asignatura = _contexto.Asignaturas.Find(seccion);
+
+            _contexto.Asignaturas.Remove(asignatura);
+            _contexto.SaveChanges();
+
+        }
+
+        /*public List<Asignatura> ObtenerAsignaturas()
+         {
+
+             _contexto.Asignaturas.ToList();
+
+          /*   var Asignatura1 = new Asignatura();
+
+             Asignatura1.Seccion = "1600";
+             Asignatura1.NumeroEdificio = "5";
+             Asignatura1.NombreAsignatura = "Lenguaje de Programacion 3";
+             Asignatura1.NombreCatedratico = "Franklin Xavier Rodriguez Santos";
+
+             var Asignatura2 = new Asignatura();
+
+             Asignatura2.Seccion = "1700";
+             Asignatura2.NumeroEdificio = "5";
+             Asignatura2.NombreAsignatura = "Base de Datos 1";
+             Asignatura2.NombreCatedratico = "Edgar Antonio Zuniga";
+
+
+
+             var ListaAsignaturas = new List<Asignatura>();
+
+             ListaAsignaturas.Add(Asignatura1);
+             ListaAsignaturas.Add(Asignatura2);
+
+             return ListaAsignaturas;
+         }*/
+    } 
 }
---
+++ CargaAcademicaInformatica/CargaAcademica.WebAdmin/Controllers/AsignaturasController.cs
@@ -0,0 +1,82 @@
+﻿using CargaAcademica.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CargaAcademica.WebAdmin.Controllers
+{
+    public class AsignaturasController : Controller
+    {
+        CargaAcademicaBL _AsignaturaBL;
+
+        public AsignaturasController()
+        {
+            _AsignaturaBL = new CargaAcademicaBL();
+        }
+
+
+
+
+        // GET: Asignaturas
+        public ActionResult Index()
+        {
+            var listadeAsignaturas = _AsignaturaBL.ObtenerAsignatura();
+
+            return View(listadeAsignaturas);
+        }
+
+        public ActionResult Crear()
+        {
+            var nuevaAsignatura = new Asignatura();
+
+            return View(nuevaAsignatura);
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Asignatura asignatura)
+        {
+            _AsignaturaBL.GuardarAsignatura(asignatura);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Editar(int seccion)
+        {
+            var asignatura = _AsignaturaBL.ObtenerAsignatura(seccion);
+
+            return View(asignatura);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Asignatura asignatura)
+        {
+            _AsignaturaBL.GuardarAsignatura(asignatura);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Detalle(int seccion)
+        {
+            var asignatura = _AsignaturaBL.ObtenerAsignatura(seccion);
+
+            return View(asignatura);
+        }
+
+        public ActionResult Eliminar(int seccion)
+        {
+            var asignatura = _AsignaturaBL.ObtenerAsignatura(seccion);
+            return View(asignatura);
+        }
+        [HttpPost]
+        public ActionResult Eliminar(Asignatura asignatura)
+        {
+            _AsignaturaBL.EliminarAsignatura(asignatura.Seccion);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
